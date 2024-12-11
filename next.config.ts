14953eb1e047cde95f014ff93f505e9fca373fa6
mohamedhany1231 <mohamedhany61@gmail.com>
2024-12-11 06:11:51 +0200

    edit next config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    //
     ignoreBuildErrors: true,
   },
-  /* config options here */
   images: {
     remotePatterns: [
       {
